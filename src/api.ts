88a3e3b19895e53ee09f0242e7ecb40ac9bb636c
Reece Balfour <reece@RBMP.local>
2025-05-12 14:26:31 +0100

    feat: update gitignore to ignore symbol linked BE types, add correct types to api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import { Task } from "@/chartsyBE-types/";
 
 // lambda api
-const api = axios.create({ baseURL: "https://fd26h64qda.execute-api.eu-west-2.amazonaws.com/api" });
+const api = axios.create({
+  baseURL: "https://fd26h64qda.execute-api.eu-west-2.amazonaws.com/api",
+});
 // const api = axios.create({ baseURL: "http://localhost:4000/api" });
 
 export const getAllTasks = async () => {
@@ -9,18 +12,25 @@ export const getAllTasks = async () => {
     const tasks = await api.get("tasks");
     console.log("FE tasks: ", tasks);
     return tasks.data.tasks;
-  } catch (err: Error) {
-    console.log(err.response, "error");
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      console.log(err.response, "error");
+    } else {
+      console.log("Unexpected error: ", err);
+    }
   }
 };
 
-export const postTask = async (task) => {
+export const postTask = async (task: Task) => {
   try {
-    
     const newTask = await api.post("task", task);
     console.log("FE newTask: ", newTask);
     return newTask.data.task;
-  } catch (err) {
-    console.log(err.response, "error");
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      console.log(err.response, "error");
+    } else {
+      console.log("Unexpected error: ", err);
+    }
   }
 };
