31dd1fce2822e2b3bdf991b34d7767da4bd56544
Reece Balfour <reece@RBMP.local>
2025-06-09 15:01:24 +0100

    add next tick to occassionaly failing test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,46 +1,49 @@
 /// <reference types="vitest" />
-import { render, screen } from '@testing-library/vue'
-import App from '../src/App.vue'
-import { vi } from 'vitest'
+import { render, screen } from "@testing-library/vue";
+import App from "../src/App.vue";
+import { vi } from "vitest";
+import { nextTick } from "vue";
 
 // Mock Device plugin
-vi.mock('@capacitor/device', () => ({
+vi.mock("@capacitor/device", () => ({
   Device: {
     getInfo: vi.fn(() =>
       Promise.resolve({
-        platform: 'mac' // You can change this to 'ios' or 'android' to test different UI branches
-      } as any)
-    )
-  }
-}))
+        platform: "mac", // You can change this to 'ios' or 'android' to test different UI branches
+      } as any),
+    ),
+  },
+}));
 
 // Mock getAllTasks API call
-vi.mock('../api', async () => {
+vi.mock("../api", async () => {
   return {
-    getAllTasks: () => Promise.resolve([
-      {
-        id: 1,
-        title: 'Mock Task',
-        description: 'Mock Description',
-        status: 'complete'
-      }
-    ]),
-    postTask: () => Promise.resolve()
-  }
-})
+    getAllTasks: () =>
+      Promise.resolve([
+        {
+          id: 1,
+          title: "Mock Task",
+          description: "Mock Description",
+          status: "complete",
+        },
+      ]),
+    postTask: () => Promise.resolve(),
+  };
+});
 
-describe('App.vue', () => {
-  it('renders platform-specific UI and tasks', async () => {
-    render(App)
-  })
+describe("App.vue", () => {
+  it("renders platform-specific UI and tasks", async () => {
+    render(App);
+  });
 
-  it('renders buttons', async () => {
-    render(App)
- const button = await screen.findByRole('button', {
-    name: /write tasks? to file/i,
-  })
+  it("renders buttons", async () => {
+    render(App);
+    await nextTick();
+    const button = await screen.findByRole("button", {
+      name: /write tasks? to file/i,
+    });
 
-  expect(button).toBeTruthy()
-    expect(screen.getByText('post dummy task')).not.toBeNull()
-  })
-})
+    expect(button).toBeTruthy();
+    expect(screen.getByText("post dummy task")).not.toBeNull();
+  });
+});
